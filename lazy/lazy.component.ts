3503441bcf054427fda875d7cae274bb849d1f98
Divyanshu1510 <Divyanshu.19CS121@dpgitm.ac.in>
2023-04-12 14:12:16 +0530

    commit

--- lazy/lazy.component.ts
+++ lazy/lazy.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { User } from '../app/_models';
 import { AccountService } from '../app/_services';
 
-@Component({ templateUrl: 'lazy.component.html' })
+@Component({
+  selector: 'app-lazy',
+  templateUrl: 'lazy.component.html'
+})
 export class LazyComponent {
-  user: User | null;
 
+  @Input() item = "";
+
+  user: User | null;
   constructor(private accountService: AccountService) {
     this.user = this.accountService.userValue;
   }
